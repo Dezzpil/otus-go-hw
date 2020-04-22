2479db6f32a34b4bcf62b68d2b3f4ec049b42576
Nikita Dezzpil Orlov <n.dezz.orlov@gmail.com>
2020-04-22 13:38:05 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	current := time.Now()
+	exact, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println("current time:", current.Round(-1))
+	fmt.Println("exact time:", exact.Round(-1))
 }
